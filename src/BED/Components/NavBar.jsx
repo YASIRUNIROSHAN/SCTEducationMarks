2bc5992ffbe69215437ba4e46677828a5eb19dda
salinda96 <salinda171@nie.edu.lk>
2022-03-15 20:11:19 +0530

    navbar

--- src/BED/Components/NavBar.jsx
+++ src/BED/Components/NavBar.jsx
@@ -1,9 +1,47 @@
+import styled from "styled-components"
 
+const Container = styled.div`
+    background-color: aliceblue;
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const Left = styled.div`
+display: flex;
+    flex: 1;
+    align-items: center;
+`;
+const Center = styled.div`
+    flex: 1;
+    align-items: center;
+`;
+const Logo = styled.h1`
+  font-weight: 300;
+`;
+
+const Right = styled.div`
+display: flex;
+        flex: 1;
+    align-items: center;
+`;
 
 const NavBar = () => {
   return (
     <Container>
-
+        <Wrapper>
+          <Left>Left</Left>
+          <Center>
+          <Logo>
+              Department 
+          </Logo>
+          </Center>
+          <Right>Right</Right>
+        </Wrapper>
     </Container>
   )
 }
